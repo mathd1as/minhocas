f01f1acdc8074623c5ea7ad4807c49513532b3a3
mathd1as <matheus.vex@gmail.com>
2021-04-28 17:28:25 -0300

    Primeiro cometa

---
+++ src/minhoca_bruta.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h> 
+#include <time.h>
+using namespace std; 
+ 
+double maximum_sum_path(vector<vector<int> > matrix, int n, int m) { 
+	int dp[n][m]; 
+	dp[0][0] = matrix[0][0]; 
+
+	/* Inicializa primeira linha do array */
+	for (int j = 1; j < m; j++) 
+		dp[0][j] = dp[0][j-1] + matrix[0][j]; 
+
+	/* Inicializa a primeira coluna da matriz dp que calcula o custo total*/
+	for (int i = 1; i < n; i++) 
+		dp[i][0] = dp[i-1][0] + matrix[i][0]; 
+       
+    /* Construir o resto da matriz dp */
+	for (int i = 1; i < n; i++){
+		for (int j = 1; j < m; j++){
+			int a = max(dp[i-1][j], dp[i][j-1]);
+			dp[i][j] = matrix[i][j] + max(dp[i-1][j], dp[i][j-1]); 	
+		}	
+	}
+	 
+    double g = (double)dp[n-1][m-1];
+	return g; 
+} 
+
+int main() {     
+	int n, m;
+	
+   	cout << "Entre com a quantidade de linhas" << endl;
+    cin >> n;
+    
+    cout << "Entre com a quantidade de colunas" << endl;
+    cin >> m;
+    
+    vector<vector<int> > matrix(n);
+  	for ( int i = 0 ; i < n ; i++ ) {
+		matrix[i].resize(m);		
+	}
+    
+    cout << "Entre com o valores da matriz" << endl;
+	for(int i = 0; i < n; i++){
+        for(int j = 0; j < m; j++){
+            cin >> matrix[i][j];
+        }   
+    }
+    
+    cout << endl;
+    cout << "Matriz";
+    for(int i = 0; i < n; i++){
+    	cout << endl;
+        for(int j = 0; j < m; j++){
+            cout << matrix[i][j] << " ";
+        }   
+    }
+    cout << endl;
+    cout << endl;	
+		
+    double b = maximum_sum_path(matrix, n, m);
+    
+    cout << "Minhocas coletadas: ";
+	cout << b << endl;
+	cout << endl;
+	
+	return 0; 
+}  	
---
+++ src/minhoca_dinamica.cpp
@@ -0,0 +1,104 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define N 100
+ 
+// Namero de linhas e colunas
+int n, m;
+ 
+// Declarando o tamanho maximo da matriz
+int a[N][N];
+ 
+vector<vector<int> > dp(N, vector<int>(N)),
+    visited(N, vector<int>(N));
+ 
+// Guarda o valor da soma corrente
+int current_sum = 0;
+ 
+//Guarda o valor da soma total
+int total_sum = 0;
+ 
+// Funcao que realiza o input dos dados
+void inputMatrix() {
+	cout << "Entre com a quantidade de linhas" << endl;
+    cin >> n;
+    
+    cout << "Entre com a quantidade de colunas" << endl;
+    cin >> m;
+    
+    cout << "Entre com o valores da matriz" << endl;
+    for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> a[i][j];	   
+		}
+	}
+	
+	cout << endl;
+	cout << "Matriz";
+    for(int i = 0; i < n; i++){
+    	cout << endl;
+        for(int j = 0; j < m; j++){
+            cout << a[i][j] << " ";
+        }   
+    }
+    cout << endl;
+    cout << endl;
+}
+ 
+// Funcao que calcula a soma dos caminhos
+int maximum_sum_path(int i, int j) {
+    // Verifica se esta no range
+    if (i == n - 1 && j == m - 1)
+        return a[i][j];
+ 
+    // Verificando se a celula ja foi visitada
+    if (visited[i][j])
+        return dp[i][j];
+ 
+    // Marcando acelula como visitada
+    visited[i][j] = 1;
+ 
+    // Atualizando o valor maximo da soma
+    int& total_sum = dp[i][j];
+ 
+    /*
+	Verifica se a posicao
+	nao visitou a ultima linha ou a ultima coluna.
+	Fazendo chamadas recursivas para todos os
+	movimentos possiveis da colula atual e em seguida, adicionando
+	o valor maximo retornado pelas chamadas e atualizando-o.
+	*/
+     if (i < n - 1 & j < m - 1) {
+     	cout << a[i][j] << " ";
+        
+        int current_sum = max(maximum_sum_path(i, j + 1),
+			maximum_sum_path(i + 1, j));
+		total_sum = a[i][j] + current_sum;
+    }
+ 
+    // Verificando se
+    // a posicao foi alcancada na ultima coluna
+    else if (i == n - 1)
+        total_sum = a[i][j]
+                    + maximum_sum_path(i, j + 1);
+ 
+    else
+        total_sum = a[i][j]
+                    + maximum_sum_path(i + 1, j);
+ 
+    return total_sum;
+}
+ 
+int main() {
+    inputMatrix();
+ 
+    cout << "Caminho do percorrido: ";
+    
+    int maximum_sum = maximum_sum_path(0, 0);
+    
+    cout << endl;
+    cout << "Minhocas coletadas: ";
+    cout << maximum_sum;
+    cout << endl;
+    
+    return 0;
+}
