9ef5b7944f243e8f758d4ba038d2c4d0c64d74d3
Matheus de Lara Dias da Silva <matheus.vex@gmail.com>
2021-04-28 17:43:53 -0300

    Aualizando identacao errada tentativa 3 (agora vai)

--- src/minhoca_bruta.cpp
+++ src/minhoca_bruta.cpp
@@ -14,7 +14,7 @@ double maximum_sum_path(vector<vector<int> > matrix, int n, int m) {
 	for (int i = 1; i < n; i++) 
 		dp[i][0] = dp[i-1][0] + matrix[i][0]; 
        
-    /* Construir o resto da matriz dp */
+    	/* Construir o resto da matriz dp */
 	for (int i = 1; i < n; i++){
 		for (int j = 1; j < m; j++){
 			int a = max(dp[i-1][j], dp[i][j-1]);
@@ -22,45 +22,44 @@ double maximum_sum_path(vector<vector<int> > matrix, int n, int m) {
 		}	
 	}
 	 
-    double g = (double)dp[n-1][m-1];
+	double g = (double)dp[n-1][m-1];
 	return g; 
 } 
 
 int main() {     
 	int n, m;
 	
-   	cout << "Entre com a quantidade de linhas" << endl;
-    cin >> n;
+	cout << "Entre com a quantidade de linhas" << endl;
+    	cin >> n;
     
-    cout << "Entre com a quantidade de colunas" << endl;
-    cin >> m;
+    	cout << "Entre com a quantidade de colunas" << endl;
+    	cin >> m;
     
-    vector<vector<int> > matrix(n);
+    	vector<vector<int> > matrix(n);
   	for ( int i = 0 ; i < n ; i++ ) {
 		matrix[i].resize(m);		
 	}
     
-    cout << "Entre com o valores da matriz" << endl;
-	for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            cin >> matrix[i][j];
-        }   
-    }
-    
-    cout << endl;
-    cout << "Matriz";
-    for(int i = 0; i < n; i++){
+	cout << "Entre com o valores da matriz" << endl;
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+		    		cin >> matrix[i][j];
+			}   
+	    	}
     	cout << endl;
-        for(int j = 0; j < m; j++){
-            cout << matrix[i][j] << " ";
-        }   
-    }
-    cout << endl;
-    cout << endl;	
+    	cout << "Matriz";
+    	for(int i = 0; i < n; i++){
+    		cout << endl;
+        	for(int j = 0; j < m; j++){
+            		cout << matrix[i][j] << " ";
+        	}   
+    	}
+    	cout << endl;
+    	cout << endl;	
 		
-    int b = maximum_sum_path(matrix, n, m);
+    	int b = maximum_sum_path(matrix, n, m);
     
-    cout << "Minhocas coletadas: ";
+    	cout << "Minhocas coletadas: ";
 	cout << b << endl;
 	cout << endl;
 	
